d26dc625bbaeaa4824569373defdad2aedffe031
Gad Test <Mgad756@gmail.com>
2020-06-27 19:30:25 -0400

    style home page first

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def new
 		@article = Article.new
 	end
@@ -14,14 +16,39 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+		
 	end
 
 	def index
 		@articles = Article.all
 	end
 
+	def edit
+
+	end
+
+	def update
+		if@article.update(article_params)
+			flash[:notice] = 'Updated'
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @article.destroy
+			flash[:notice] = "article was successfully deleted"
+			redirect_to articles_path
+		else
+		end
+	end
+
  private
+
+ 	def set_article
+ 		@article = Article.find(params[:id])
+ 	end
 
  	def article_params
 		params.require(:article).permit(:title, :description)
